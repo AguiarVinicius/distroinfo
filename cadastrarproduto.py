3d537f839b4adcf18f2f3b223e43593db30519fd
AguiarVinicius <126164707+AguiarVinicius@users.noreply.github.com>
2023-11-09 09:00:26 -0400

    Update cadastrarproduto.py
    Atualização Modulo de cadastrar produto

--- cadastrarproduto.py
+++ cadastrarproduto.py
@@ -1,8 +1,7 @@
 
-
 def cadastrarproduto ():
 
-        nome = input("Nome : ")
+        nome = input("Nome: ")
         valor = input("Valor: ")
         quantidade = input("Quantidade: ")
         tipo = input("Tipo: ")
@@ -18,4 +17,13 @@ def cadastrarproduto ():
         return sub_produto
 
 produto_cad = cadastrarproduto ()
+
+#adicionar o novo produto em um arquivo txt
+with open ("produto_cad.txt","a") as arquivo:
+        # Use "a" para adicionar ao arquivo existente
+        produto_cad_str = f"{produto_cad['nome']},{produto_cad['valor']},{produto_cad['quantidade']},{produto_cad['tipo']},{produto_cad['data']}\n"
+
+        arquivo.write(produto_cad_str)
+
 print("Produto",produto_cad )
+
